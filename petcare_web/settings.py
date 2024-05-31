dd5f20aebcb284e28849b2b0ca8e6bc151775386
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-05-31 17:32:47 -0300

    refact/build: add static_root on settings.py - whitenoise config

--- petcare_web/settings.py
+++ petcare_web/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
