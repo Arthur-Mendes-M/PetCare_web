5bd02d4b256d90cbd6ef2083bcd23ba450048ba0
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-06-02 18:57:31 -0300

    feat/refact: split script codes in a single file, add sytyle links on js file and finishing build mobile version of base template.

---
+++ templates/static/scripts/base.js
@@ -0,0 +1,33 @@
+lucide.createIcons();
+
+let hamburger_menu = document.getElementById("hamburger_menu")
+let hamburger_menu_middle_bar = document.querySelector("#hamburger_menu span")
+
+let secondary_menu = document.querySelector(".secondary_menu")
+
+let logout_button = document.getElementById("logout")
+
+let all_menu_links = document.querySelectorAll('#side_menu nav ul li, .secondary_menu nav ul li')
+
+all_menu_links.forEach((link, index) => {
+    let page = link.getAttribute('data-page')
+
+    link.classList.remove('current')
+
+    if (window.location.pathname.endsWith(page)) {
+        all_menu_links[index].classList.add('current')
+    }
+})
+
+hamburger_menu.addEventListener('click', () => {
+    hamburger_menu_middle_bar.classList.toggle('show')
+    secondary_menu.classList.toggle('show')
+})
+
+logout_button.addEventListener('click', () => {
+    let confirmation = confirm("Você realmente deseja sair do sistema?")
+
+    if (confirmation) {
+        window.location.href = '/logout'
+    }
+})
