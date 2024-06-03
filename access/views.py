63badf750296e2e3869cba7056c7a4946f84a6b0
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-06-02 22:03:42 -0300

    refact/build: remove all django DB dependencies and refact to just use cookies.

--- access/views.py
+++ access/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.urls import reverse
 from dotenv import load_dotenv
 
 from utils.session_handler import session_handler
 from utils.API import Employees
 
+import ast
+
 # Load environment variables from .env file
 load_dotenv()
 
-def login(request):
+def login(request):    
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -21,8 +22,7 @@ def login(request):
             session_handler(request, 'employee', api_data[0], 'set')
             response = redirect(reverse('home'))
         else:
-            messages.error(request, api_data['description'])
-            response = render(request, 'login.html')
+            response = render(request, 'login.html', {'login_error': api_data['description']})
             
         return response
 
@@ -41,7 +41,22 @@ def signup(request):
             'image': request.FILES['image'] if request.FILES and request.FILES['image'] else None
         }
     
+        # TEMP
         Employees().save_one(data=data, files=file)
+
+        # UPDATE THE LOGIC BELOW - SIMPLIFY
+        # FIX THE PROBLEMS
+        # response = Employees().save_one(data=data, files=file)
+        # response_to_obj = ""
+
+        # if 'error' in response.json():
+        #     response_to_obj = ast.literal_eval(response.json()["error"])
+        # elif 'description' in response.json() and 'megabytes' in response.json()['description']:
+        #     return render(request, 'signup.html', {"signup_error": "Imagem muito grande. Utilize imagens de até 3M e com extensões .jpeg, .jpg ou .png"})
+
+        # if 'code' in response_to_obj and response_to_obj['code'] == '23505':
+        #     return render(request, 'signup.html', {"signup_error": "Esse e-mail já existe! Utilize outro."})
+
 
         return redirect(reverse('login'))
 
--- petcare_web/settings.py
+++ petcare_web/settings.py
@@ -30,25 +30,21 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
+# CHANGED TO NOT USE A DEPENDENCIES WITH DB
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'access',
-    'home'
+    'home',
 ]
 
+# CHANGED TO NOT USE A DEPENDENCIES WITH DB
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'petcare_web.middlewares.AuthenticateRoutes',
     'petcare_web.middlewares.ErrorHandler'
@@ -56,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'petcare_web.urls'
 
+# CHANGED TO NOT USE A DEPENDENCIES WITH DB
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -64,9 +61,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
@@ -78,34 +73,20 @@ WSGI_APPLICATION = 'petcare_web.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# CHANGED TO USE A FAKE DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy'
+    }
+}
 
-DATABASES = {}
-
+# ADDED FOR CHANGE SESSION STORAGE LOGIC TO NOT USE DB
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
+# REMOVED BECAUSE DB IS NOT USED
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
