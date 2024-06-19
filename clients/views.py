f1d00b1383826062cde10cac4e1171609534cee7
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-06-19 00:16:42 -0300

    feat: inializing the clients page. clients have been listed. PENDING: split table codes, logic of clients page.

--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from utils.API import Clients
 import random
+
 def clients(request):
     random_number = random.randint(1, 7)
-    return render(request, 'clients/index.html', context={'random_number': random_number})
+    clients_list = Clients().get_all().json()
+
+    return render(request, 'clients/index.html', context={'clients_list': clients_list, 'random_number': random_number})
--- utils/API.py
+++ utils/API.py
@@ -57,3 +57,52 @@ class Employees:
             url=self.build_url(id)
         )
 
+class Clients:
+    @abstractmethod
+    def build_url(self, id=None):
+        if not id:
+            return f"{os.getenv('PETCARE_API_URL')}clients/?auth={os.getenv('PETCARE_AUTH_TOKEN')}"
+        elif id:
+            return f"{os.getenv('PETCARE_API_URL')}clients/{id}?auth={os.getenv('PETCARE_AUTH_TOKEN')}"
+    
+    @abstractmethod
+    def get_all(self):
+        return requests.get(
+            url=self.build_url()
+        )
+
+    @abstractmethod    
+    def get_one(self, id):
+        return requests.get(
+            url=self.build_url(id)
+        )
+    
+    @abstractmethod    
+    def validate(self, data):
+        return requests.get(
+            url=self.build_url(),
+            headers=json_header,
+            data=json.dumps(data)
+        )
+
+    @abstractmethod
+    def save_one(self, data, files):            
+        return requests.post(
+            url=self.build_url(),
+            data=data,
+            files=files
+        )
+    
+    @abstractmethod
+    def update_one(self, id, new_data, new_files=None):
+        return requests.put(
+            url=self.build_url(id),
+            data=new_data,
+            files=new_files
+        )
+    
+    @abstractmethod
+    def delete_one(self, id):
+        return requests.delete(
+            url=self.build_url(id)
+        )
