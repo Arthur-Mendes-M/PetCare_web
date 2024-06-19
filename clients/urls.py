8be230b8bcfaa43c9f8e4359125f9968402dd779
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-06-19 18:19:01 -0300

    feat: clients page was finished

--- clients/urls.py
+++ clients/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.clients, name='clients')
+    path('', views.clients, name='clients'),
+    path('/create_client', views.create_client, name='create_client'),
+    path('/edit_client', views.edit_client, name='edit_client'),
+    path('/delete_client', views.delete_client, name='delete_client'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse
 from utils.API import Clients
 import random
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+
 
 def clients(request):
     random_number = random.randint(1, 7)
     clients_list = Clients().get_all().json()
 
     return render(request, 'clients/index.html', context={'clients_list': clients_list, 'random_number': random_number})
+
+def create_client(request): 
+    files = request.FILES if (request.FILES and 'image' in request.FILES) else None
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    birthday = request.POST.get('birthday')
+    cep = request.POST.get('cep')
+    neighborhood = request.POST.get('neighborhood')
+    street = request.POST.get('street')
+    number = request.POST.get('street_number')
+    state = request.POST.get('state')
+    city = request.POST.get('city')
+
+    client = {
+        "name": name,
+        "email": email,
+        "password": password,
+        "birthday": birthday,
+        "address": json.dumps({
+            "cep": cep,
+            "neighborhood": neighborhood,
+            "street": street,
+            "street_number": number,
+            "state": state,
+            "city": city
+        })
+    }
+
+    Clients().save_one(data=client, files=files)
+
+    return redirect(reverse('clients'))
+
+@csrf_exempt
+def edit_client(request):
+    files = request.FILES if (request.FILES and 'image' in request.FILES) else None
+    client_id = request.POST.get('id')
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    birthday = request.POST.get('birthday')
+    cep = request.POST.get('cep')
+    neighborhood = request.POST.get('neighborhood')
+    street = request.POST.get('street')
+    number = request.POST.get('street_number')
+    state = request.POST.get('state')
+    city = request.POST.get('city')
+
+    client = {
+        "name": name,
+        "email": email,
+        "password": password,
+        "birthday": birthday,
+        "address": json.dumps({
+            "cep": cep,
+            "neighborhood": neighborhood,
+            "street": street,
+            "street_number": number,
+            "state": state,
+            "city": city
+        })
+    }
+
+    Clients().update_one(client_id, client, files)
+
+    return redirect(reverse('clients'))
+
+
+@csrf_exempt
+def delete_client(request):
+    clients_id = request.GET.get('clients_id')
+    id_list = None
+
+    if clients_id and ',' in clients_id:
+        id_list = clients_id.split(',')
+
+    if id_list != None:
+        for id in id_list:
+            Clients().delete_one(id)
+    else:
+        Clients().delete_one(clients_id)
+
+    return redirect(reverse('clients'))
